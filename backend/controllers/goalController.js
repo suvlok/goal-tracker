93873647c213509837ee33316c9291fe3bd40726
suvlok <suvanloku@hotmail.com>
2022-11-26 19:12:41 -0500

    added routes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,41 @@
+// @desc   Gets the goals
+// @route  GET /api/goals
+// @access Private
+const getGoals = (req, res) => {
+    res.status(200).json({ message: "Get goal"})
+}
+
+// @desc   Set a goal
+// @route  POST /api/goals
+// @access Private
+const setGoal = (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error("Please add a text field")
+    }
+
+    res.status(200).json({ message: "Set goal"})
+}
+
+// @desc   Update the goal
+// @route  PUT /api/goals/:id
+// @access Private
+const updateGoal = (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}`})
+}
+
+// @desc   Delete the goal
+// @route  DELETE /api/goals/:id
+// @access Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+}
+
+module.exports = {
+    getGoals, 
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
+
+//29:50
