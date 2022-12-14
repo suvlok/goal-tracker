ba7bd6c64354d08cf39fc6b294af1fbd1d0f3bf6
suvlok <suvanloku@hotmail.com>
2022-12-13 21:29:26 -0500

    added the protection for each of the routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController")
 
-router.route("/").get(getGoals).post(setGoal)
-router.route("/:id").delete(deleteGoal).put(updateGoal)
+const {protect} = require("../middleware/authMiddleware")
 
-// router.get("/", getGoals) 
+router.route("/").get(protect, getGoals).post(protect, setGoal) //when you add protect, you need a token
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal)
+
+// router.get("/", getGoals)            //The two lines above are a better, more concise, version of this
 
 // router.post("/", setGoal)
 
