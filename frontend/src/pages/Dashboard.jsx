b3c6f9d85f9d48e9040cdb96d9073a5e8e0f1a62
suvlok <suvanloku@hotmail.com>
2022-12-26 14:20:06 -0500

    added the delete goal functionality to the frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import {useEffect} from "react"
 import { useNavigate } from 'react-router-dom'
 import {useSelector, useDispatch} from "react-redux"
 import GoalForm from '../components/GoalForm'
-//import GoalItem from '../components/GoalItem'
+import GoalItem from '../components/GoalItem'
 import Spinner from "../components/Spinner"
 import { getGoals, reset } from '../features/goals/goalSlice'
 
@@ -42,8 +42,18 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+
       <GoalForm />
 
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal = {goal}/>
+            ))}
+          </div>
+        ) : (<h3>You have not set any goals</h3>)}
+      </section>
      
     </>
   )
